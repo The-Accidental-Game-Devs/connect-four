6ad699a7674f5edcdece344033d0e58a6f1121e7
Aung <aung.lin.thant.address@gmail.com>
2025-09-26 13:37:34 +0630

    feat(connect-four): Setup bevy basic window

--- connect-four/src/main.rs
+++ connect-four/src/main.rs
@@ -1,5 +1,21 @@
-use connect_four_engine::cli_game::player_vs_bot;
+use bevy::prelude::*;
 
 fn main() {
-    player_vs_bot(8);
+    App::new()
+        .insert_resource(ClearColor(Color::srgb(1.0, 1.0, 1.0)))
+        .add_plugins((DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Connect Four".into(),
+                name: Some("Connect Four".into()),
+                resolution: (1280.0, 720.0).into(),
+                ..default()
+            }),
+            ..default()
+        }),))
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d);
 }
