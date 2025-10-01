4ef8b207eaa49f910bab99cf82130e92491839af
Aung <aung.lin.thant.address@gmail.com>
2025-10-01 13:44:08 +0630

    refactor: Change the size of image assets

--- connect-four/src/settings.rs
+++ connect-four/src/settings.rs
@@ -1,7 +1,7 @@
-pub const PIECE_SIZE: f32 = 64.0;
+pub const PIECE_SIZE: f32 = 96.0;
 pub const HALF_PIECE_SIZE: f32 = PIECE_SIZE / 2.0;
-pub const BOARD_WIDTH: f32 = 448.0;
-pub const BOARD_HEIGHT: f32 = 384.0;
+pub const BOARD_WIDTH: f32 = 672.0;
+pub const BOARD_HEIGHT: f32 = 576.0;
 pub const HALF_BOARD_WIDTH: f32 = BOARD_WIDTH / 2.0;
 pub const HALF_BOARD_HEIGHT: f32 = BOARD_HEIGHT / 2.0;
 pub const GRAVITY: f32 = 1200.0;
